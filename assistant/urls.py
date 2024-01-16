2cd71790c2918ae5112fb35b96fd355ab4abbcbe
Julia Farganus <juliafarganus@gmail.com>
2024-01-16 14:34:36 +0100

    NEW REQUIREMENTS

--- assistant/urls.py
+++ assistant/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
 from . import views
+from .views import GeneratePlanController
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:student_id>/", views.student, name="student")
+    path("<int:student_id>/", GeneratePlanController.student, name="student")
 ]
--- assistant/views.py
+++ assistant/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
 
 from assistant.models import Timetable, EnrollmentRecord, Group
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class GeneratePlanController:
 
+    @staticmethod
+    # /assistant/266593/
+    def student(request, student_id):
+        try:
+            student_timetable = Timetable.objects.get(student_id=student_id)
+        except Timetable.DoesNotExist:
+            raise Http404("Question does not exist")
 
-# /assistant/266593/
-def student(request, student_id):
-    student_timetable = Timetable.objects.get(student_id=student_id)
-    enrollment_records = EnrollmentRecord.objects.filter(timetable=student_timetable.id).all()
+        enrollment_records = EnrollmentRecord.objects.filter(timetable=student_timetable.id).all()
 
-    student_groups = [enr.group_code for enr in enrollment_records]
+        context = {
+            'student_groups': [enr.group_code for enr in enrollment_records]
+        }
 
-    template = loader.get_template('assistant/student.html')
-    context = {
-        'student_groups': student_groups
-    }
+        return render(request, 'assistant/student.html', context)
 
-    return HttpResponse(template.render(context, request))
